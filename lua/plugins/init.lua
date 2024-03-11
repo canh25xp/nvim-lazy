498b4bce4fdf6716ace2abbe98ad5aec3e2cd077
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-12 00:00:07 +0700

    custom plugins

--- lua/plugins/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- disable noice
-  { "folke/noice.nvim", enabled = false },
-}
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<C-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+        shell = "bash --login",
+      })
+    end,
+    cmd = "ToggleTerm",
+    keys = {
+      { [[<C-\>]] },
+      { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+      {
+        "<leader>th",
+        "<cmd>ToggleTerm dir=~ direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at homedir",
+      },
+    },
+  },
+}
