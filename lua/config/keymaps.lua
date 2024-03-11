1d3184420166f0118a01c565133fcef3bd17051a
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-11 15:41:42 +0700

    add key binddings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local nomap = vim.keymap.del
+
+map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
