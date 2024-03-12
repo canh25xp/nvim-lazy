91a69638c1b39ce3293165ae5dc0a6961649d5eb
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-13 02:35:07 +0700

    config toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,6 @@ return {
     config = function()
       require("toggleterm").setup({
         open_mapping = [[<C-\>]],
-        shade_terminals = true,
         -- add --login so ~/.zprofile is loaded
         -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
         -- shell = "bash --login",
@@ -14,11 +13,6 @@ return {
     keys = {
       { [[<C-\>]] },
       { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
-      {
-        "<leader>th",
-        "<cmd>ToggleTerm dir=~ direction=horizontal<cr>",
-        desc = "Open a horizontal terminal at homedir",
-      },
     },
   },
 }
