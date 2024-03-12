2d4fdab5b532a226c50c63586a89a8159d636887
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-12 20:45:37 +0700

    change theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,11 +2,26 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
+      style = "moon",
       transparent = true,
+      terminal_colors = true,
       styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
       },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
     },
   },
 }
