7e004c1b8c1482554f7a77f1a29f6e282d6ec0c9
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-13 22:53:42 +0700

    disable hide tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,12 @@ return {
         ["l"] = "open",
       },
     },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false, -- only works on Windows for hidden files/directories
+      },
+    },
   },
 }
