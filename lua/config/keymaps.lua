50ff40605695e2d2880cd3254439dc7dd81f5c1d
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-17 00:15:15 +0700

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,4 +11,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 map("n", "<tab>", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 
+-- map("n", "<C-b>", "<cmd>colorscheme habamax<cr>", { desc = "" })
+
+map("n", "<C-b>", "<leader>fe", { desc = "Explorer NeoTree (root dir)", remap = true })
+
 -- nomap("n", "<leader>gg")
