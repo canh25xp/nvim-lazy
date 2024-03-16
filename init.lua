d2392006daf5ee9934621daded908d17c92458af
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-16 16:13:38 +0700

    bruh

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 if not vim.g.vscode then
+  -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
 else
-  print("bruh")
+  print("neovim-vscode config")
 end
