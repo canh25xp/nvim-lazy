530b324a6f3272845c365145e1e7941dff21f65a
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-17 10:57:52 +0700

    add bracket

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,17 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    close_if_last_window = true,
-    window = {
-      mappings = {
-        ["l"] = "open",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      window = {
+        mappings = {
+          ["l"] = "open",
+        },
       },
-    },
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = true,
-        hide_gitignored = false,
-        hide_hidden = false, -- only works on Windows for hidden files/directories
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = true,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+        },
       },
     },
   },
