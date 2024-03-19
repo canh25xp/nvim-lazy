b56eafe4384a52108f1d54872eec34827503b693
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-20 00:10:43 +0700

    remove custom lsp

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -3,14 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        -- pyright = {},
-        html = {},
-        emmet_ls = {},
-        emmet_language_server = {},
-        -- yamlls = {},
+        -- html = {},
+        -- emmet_ls = {},
+        -- emmet_language_server = {},
       },
     },
   },
