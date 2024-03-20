5ef1c18646127eac47744ff3221c782e251e85c3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-20 14:37:32 +0700

    organize plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,45 +17,4 @@ return {
       },
     },
   },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "htmldjango",
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        ["htmldjango"] = { "djlint" },
-      },
-      formatters = {
-        djlint = {
-          args = { "--reformat", "-" },
-          cwd = require("conform.util").root_file({
-            ".djlintrc",
-          }),
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        htmldjango = { "djlint" },
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "djlint",
-      })
-    end,
-  },
 }
