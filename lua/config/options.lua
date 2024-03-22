bc4c0c0d37ec17d0027ccdc7f4615b0419c51c71
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-22 11:14:23 +0700

    disable autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.maplocalleader = ","
-
 local opt = vim.opt
+local g = vim.g
+
+g.maplocalleader = ","
+g.autoformat = false
 
 opt.relativenumber = true
 opt.cursorlineopt = "number"
