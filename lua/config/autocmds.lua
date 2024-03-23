cf5edb4e8d6ca947448146fbb24b0a704900589f
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-24 00:32:59 +0700

    cay the nho

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,13 +3,4 @@
 -- Add any additional autocmds here
 local api = vim.api
 
-api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
-  pattern = { "*" },
-  callback = function()
-    if vim.opt.buftype:get() == "terminal" then
-      vim.cmd(":startinsert")
-    end
-  end,
-})
-
 api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
