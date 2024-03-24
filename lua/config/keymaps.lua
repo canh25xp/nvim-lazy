3135e4219fff447416642f567dced0a9924f1920
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-24 20:57:15 +0700

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,7 +42,6 @@ map("n", "<leader>gl", glow, { desc = "Open markdown file"})
 map("n", "<leader>gg", lazygit, { desc = "Lazygit (root dir)" })
 
 -- Terminal
-nomap("t", "<esc><esc>")
 -- map("t", "jk", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
 map("t", "<C-x>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
 map("n", "<C-_>", toggle_term, { desc = "Terminal (root dir )" }) -- Auto translate to <C-/>
