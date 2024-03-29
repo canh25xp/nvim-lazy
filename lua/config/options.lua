0fda69145466fd9109a1ade95d1eaa5099cd84f1
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-29 08:57:03 +0700

    disable root detection

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,7 @@ local g = vim.g
 
 g.maplocalleader = "\\"
 g.autoformat = false
+g.root_spec = { "cwd" }
 
 opt.linebreak = true
 opt.timeoutlen = 1000
@@ -14,6 +15,7 @@ opt.relativenumber = true
 opt.cursorlineopt = "number"
 opt.conceallevel = 0
 opt.wildmode = "list:full" -- Command-line completion mode
+
 
 -- Set shell to PowerShell 7 if on Win32 or Win64
 if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
