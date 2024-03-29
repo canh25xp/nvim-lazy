803526655e273ab8fbcc1a0f35b9d8ebde5c1484
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-29 09:38:05 +0700

    Revert "lazyvim now auto config for pwsh"
    This reverts commit 2bc68dd862b91ea0dc979d860b31829830f21b9b.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,12 +18,12 @@ opt.wildmode = "list:full" -- Command-line completion mode
 
 
 -- Set shell to PowerShell 7 if on Win32 or Win64
--- if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
---   opt.shell = "pwsh"
---   opt.shellcmdflag =
---     "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
---   opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
---   opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
---   opt.shellquote = ""
---   opt.shellxquote = ""
--- end
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  opt.shell = "pwsh"
+  opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  opt.shellquote = ""
+  opt.shellxquote = ""
+end
