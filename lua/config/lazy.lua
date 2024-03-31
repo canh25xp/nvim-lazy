8d756c4a4859a001e71009cc6e70e81e8de8f027
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-31 11:26:14 +0700

    add extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.editor.mini-diff" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lazyrc" },
     -- import/override with your plugins
