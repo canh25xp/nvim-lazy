1966cae779150b4cf69ea45b0334d9d9134e2aad
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-31 17:05:07 +0700

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -11,7 +11,6 @@ return {
     opts = {
       style = "storm",
       transparent = true,
-      dim_inactive = true,
       terminal_colors = true,
       styles = {
         sidebars = "dark", -- style for sidebars, see below
@@ -25,11 +24,6 @@ return {
     name = "catppuccin",
     opts = {
       transparent_background = false, -- disables setting the background color.
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.5, -- percentage of the shade to apply to the inactive window
-      },
     },
   },
   {
