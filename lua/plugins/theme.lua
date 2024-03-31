78e4e3b2404db6a11c2177c507e244aedc1d0bda
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-31 11:53:42 +0700

    add theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,16 +6,29 @@ else
 end
 
 return {
-  -- Config tokyonight
   {
     "folke/tokyonight.nvim",
     opts = {
       style = "storm",
       transparent = true,
+      dim_inactive = true,
       terminal_colors = true,
       styles = {
         sidebars = "dark", -- style for sidebars, see below
         floats = "dark", -- style for floating windows
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = false, -- disables setting the background color.
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.5, -- percentage of the shade to apply to the inactive window
       },
     },
   },
