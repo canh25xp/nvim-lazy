ee07d5d95ce0bc18107be4d564a7d993edc4cf18
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-01 17:02:03 +0700

    remove mini-diff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.editor.mini-diff" },
+    -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.lazyrc" },
     -- import/override with your plugins
     { import = "plugins" },
