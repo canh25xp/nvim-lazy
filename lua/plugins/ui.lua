64cd32f71fb578d5dc6eac6993a4fbe31ead2e84
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-04 05:08:12 +0700

    turn off mini indentscope animation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -34,9 +34,7 @@ return {
     "echasnovski/mini.indentscope",
     opts = {
       draw = {
-        animation = function()
-          return 0
-        end,
+        animation = require("mini.indentscope").gen_animation.none(),
       },
     },
   },
