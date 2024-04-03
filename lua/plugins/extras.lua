4f03514d781ac44e69dbc37ed11fa41516f499fd
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-04 04:41:57 +0700

    vimtex now don't override conceallevel any more

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "lervag/vimtex",
-    config = function()
-      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
-      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-      vim.g.tex_flavor = "latex"
-      -- vim.g.tex_conceal = "abdmgs"
-      vim.g.vimtex_quickfix_mode = 0
-      -- vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
-      -- vim.g.vimtex_view_enabled = 0
-      vim.g.vimtex_view_automatic = 0
-    end,
-  },
+  -- {
+  --   "lervag/vimtex",
+  --   config = function()
+  --     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+  --     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+  --     vim.g.tex_flavor = "latex"
+  --     -- vim.g.tex_conceal = "abdmgs"
+  --     vim.g.vimtex_quickfix_mode = 0
+  --     -- vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
+  --     -- vim.g.vimtex_view_enabled = 0
+  --     vim.g.vimtex_view_automatic = 0
+  --   end,
+  -- },
   {
     "mfussenegger/nvim-lint",
     -- enabled = false,
