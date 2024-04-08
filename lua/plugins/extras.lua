762913ed38d0709792bfcc42e84d8857dd468868
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-08 19:43:50 +0700

    config vimtex

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -7,9 +7,11 @@ return {
       vim.g.tex_flavor = "latex"
       -- vim.g.tex_conceal = "abdmgs"
       vim.g.vimtex_quickfix_mode = 0
+      -- vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
       -- vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
-      -- vim.g.vimtex_view_enabled = 0
+      vim.g.vimtex_view_enabled = 1
       vim.g.vimtex_view_automatic = 0
+      vim.g.vimtex_doc_handlers = {'vimtex#doc#handlers#texdoc'}
     end,
   },
   {
