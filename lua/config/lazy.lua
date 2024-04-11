f545a75252913b4579a3d52a7f32bba72089f0c7
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-12 01:45:51 +0700

    config default extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,10 +16,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.tex" },
+    -- { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.util.chezmoi" },
+    -- { import = "lazyvim.plugins.extras.util.chezmoi" },
     -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.lazyrc" },
     -- import/override with your plugins
