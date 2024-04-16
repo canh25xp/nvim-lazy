d8ebc7ff93e644173119199d65ae7d5dda2bbfec
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-16 17:27:45 +0700

    turn off dev mode

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = { -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", dev = true },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", dev = false },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
