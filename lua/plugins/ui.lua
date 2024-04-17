e4887f775c42e124b3c3824ec429af196d9788b3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-17 18:09:12 +0700

    lualine theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,6 +11,37 @@ local logo = [[
 
 logo = string.rep("\n", 8) .. logo .. "\n\n"
 
+-- stylua: ignore
+local colors = {
+  blue   = '#89b4fa',
+  cyan   = '#94e2d5',
+  black  = '#45475a',
+  white  = '#eff1f5',
+  red    = '#f38ba8',
+  violet = '#cba6f7',
+  grey   = '#303446',
+}
+
+local catppuccin = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = {
+    a = { fg = colors.black, bg = colors.blue },
+  },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -84,7 +115,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        theme = "dracula",
+        theme = catppuccin,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
       },
