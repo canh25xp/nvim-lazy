a69dcad83d96d906a186f02ca9d9fee4b33196a2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-19 01:06:02 +0700

    turn off spell

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- local api = vim.api
+local api = vim.api
 
--- api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
