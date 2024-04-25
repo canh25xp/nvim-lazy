b629a22c64a29616bfb437bceff212c219bd6f6d
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-25 16:45:50 +0700

    config vimtex

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -5,7 +5,7 @@ return {
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
       vim.g.vimtex_compiler_latexmk = {
-        continuous = 0,
+        continuous = 1,
       }
       -- vim.g.tex_conceal = "abdmgs"
       vim.g.vimtex_quickfix_mode = 0
