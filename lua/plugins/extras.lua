debf234fd5e729f698b069996b31062af26093ee
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-06 23:34:19 +0700

    config vimtex viewer

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -20,6 +20,9 @@ return {
         vim.g.vimtex_view_sioyek_exe = "sioyek.exe"
         vim.g.vimtex_callback_progpath = "wsl nvim"
       end
+      -- vim.g.vimtex_view_sioyek_options = "--reuse-window"
+
+      -- vim.g.vimtex_callback_progpath = "pwsh"
       -- if vim.fn.has("win32") or (vim.fn.has("unix") and vim.fn.exists("$WSLENV")) then
       --   if vim.fn.executable("sioyek.exe") then
       --     vim.g.vimtex_view_method = "sioyek"
