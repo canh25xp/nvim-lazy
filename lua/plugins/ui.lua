7cd16dbfb3d6879ab676efe5b181a27feff5822e
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-05-19 08:41:52 +0700

    update plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -62,14 +62,6 @@ return {
     },
   },
   {
-    "echasnovski/mini.indentscope",
-    opts = {
-      draw = {
-        animation = require("mini.indentscope").gen_animation.none(),
-      },
-    },
-  },
-  {
     "rcarriga/nvim-notify",
     keys = {
       {
