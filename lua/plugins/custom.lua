904afa31e65a9a852e20170411caf9626aa2fe65
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-08 01:51:32 +0700

    config vim-tmux-nav

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,6 +1,12 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_disable_when_zoomed = 0
+      vim.g.tmux_navigator_preserve_zoom = 1
+      vim.g.tmux_navigator_no_wrap = 1
+    end,
     cmd = {
       "TmuxNavigateLeft",
       "TmuxNavigateDown",
@@ -9,11 +15,11 @@ return {
       "TmuxNavigatePrevious",
     },
     keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      -- { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+      -- { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
     },
   },
   {
