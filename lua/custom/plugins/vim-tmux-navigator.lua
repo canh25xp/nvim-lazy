610f93856fe0e4b21b84a20d877eddb7e774df2e
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-09 20:36:14 +0700

    vim_tmux config

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -2,9 +2,9 @@ return {
   "christoomey/vim-tmux-navigator",
   init = function()
     vim.g.tmux_navigator_no_mappings = 1
-    vim.g.tmux_navigator_disable_when_zoomed = 1
-    vim.g.tmux_navigator_preserve_zoom = 0
-    vim.g.tmux_navigator_no_wrap = 1
+    vim.g.tmux_navigator_disable_when_zoomed = 0
+    vim.g.tmux_navigator_preserve_zoom = 1
+    vim.g.tmux_navigator_no_wrap = 0
   end,
   cmd = {
     "TmuxNavigateLeft",
