fa034cd7856cdd41bad9149a5fc76167379bc9af
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-11 11:23:39 +0700

    move plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.util.chezmoi" },
     -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "plugins" },
-    { import = "custom/plugins" },
+    { import = "plugins/extras" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
