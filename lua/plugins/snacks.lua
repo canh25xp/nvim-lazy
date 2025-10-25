b713fbb40f1ddc3a07cd638d981e4f70923d67ac
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-10-25 19:07:07 +0000

    dashboard: change Restore Session and Recent Files key
    Before:
    s: Restore Session
    r: Recent Files
    After:
    r: Restore Session
    .: Recent Files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,5 +15,23 @@ return {
         },
       },
     },
+    dashboard = {
+      preset = {
+        -- Override default keys Recent Files and Restore Session
+        -- TODO: is there a better way to do this ? I don't want to redefine every keys
+        -- stylua: ignore
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = ".", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "r", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
   },
 }
