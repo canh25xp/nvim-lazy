52462bfcb3191fd4c5a3c4ad0d4050b0bb9dd138
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-10-25 09:53:18 +0000

    format code

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,20 +19,19 @@ opt.smoothscroll = false
 opt.shiftwidth = 4
 
 opt.fillchars = {
-  horiz = '━',
-  horizup = '┻',
-  horizdown = '┳',
-  vert = '┃',
-  vertleft  = '┫',
-  vertright = '┣',
-  verthoriz = '╋',
+  horiz = "━",
+  horizup = "┻",
+  horizdown = "┳",
+  vert = "┃",
+  vertleft = "┫",
+  vertright = "┣",
+  verthoriz = "╋",
 }
 
 -- Set shell to PowerShell 7 if on Win32 or Win64
 if not vim.fn.has("win32") or not vim.fn.has("win64") then
   opt.shell = "pwsh"
-  opt.shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
   opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
   opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
   opt.shellquote = ""
