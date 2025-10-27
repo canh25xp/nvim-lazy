f5dcb5038967e990c8b834dc169e66421270aa62
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-10-27 13:26:53 +0700

    os specific options

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
+vim.g.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+vim.g.is_linux = vim.fn.has("unix") == 1
+vim.g.is_android = vim.fn.has("android") == 1
+vim.g.is_wsl = vim.fn.has("wsl") == 1
+vim.g.is_mac = vim.fn.has("mac") == 1
+vim.g.path_sep = vim.g.is_windows and ";" or ":"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,16 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-local g = vim.g
+vim.g.autoformat = false
+vim.g.root_spec = { "cwd" }
 
-g.autoformat = false
-g.root_spec = { "cwd" }
+vim.opt.conceallevel = 0
+vim.opt.winborder = "rounded"
 
-opt.conceallevel = 0
-opt.winborder = "rounded"
-
-opt.fillchars = {
+vim.opt.fillchars = {
   horiz = "━",
   horizup = "┻",
   horizdown = "┳",
@@ -21,21 +18,19 @@ opt.fillchars = {
   verthoriz = "╋",
 }
 
--- Set shell to PowerShell 7 if on Win32 or Win64
-if not vim.fn.has("win32") or not vim.fn.has("win64") then
-  opt.shell = "pwsh"
-  opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-  opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-  opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-  opt.shellquote = ""
-  opt.shellxquote = ""
+if vim.g.is_windows then
+  vim.o.shell = "pwsh"
+  vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
 end
 
-if g.neovide then
-  -- o.guifont = "CaskaydiaCove Nerd Font:h12"
-  g.neovide_fullscreen = false
-  g.neovide_scroll_animation_length = 0.3
-  g.neovide_cursor_vfx_mode = "sonicboom"
-  g.neovide_hide_mouse_when_typing = true
-  -- g.neovide_transparency = 0.8
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_transparency = 1.0
 end
