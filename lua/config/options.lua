4d2a6d895b7915a92ef5afd46f7b552c9ea2c213
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-10-27 19:40:51 +0700

    neovide: replace deprecated option transperency > opacity

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -32,5 +32,5 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.3
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_transparency = 1.0
+  vim.g.neovide_opacity = 1.0
 end
