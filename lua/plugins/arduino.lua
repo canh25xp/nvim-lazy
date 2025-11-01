9419f4df7168f0186c60b27d8318476733f565cf
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-11-01 18:58:13 +0000

    arduino: OS specific config path

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -6,7 +6,7 @@ return {
         arduino_language_server = {
           cmd = {
             "arduino-language-server",
-            "-cli-config", vim.fn.expand("~/Appdata/Local/Arduino15/arduino-cli.yaml"),
+            "-cli-config", vim.fn.expand(vim.g.is_windows and "~/AppData/Local/Arduino15/arduino-cli.yaml" or "~/.arduino15/arduino-cli.yaml"),
             "-fqbn", "arduino:avr:uno",
             "-cli", "arduino-cli",
             "-clangd", "clangd",
