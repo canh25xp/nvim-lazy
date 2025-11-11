a3bb46bced44b8cb2aee159be68fb65d810f87c3
Ngô Văn Cảnh <vancanh.ng@samsung.com>
2025-11-11 20:02:25 +0700

    lualine: y section display searchcount and selectioncount

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     sections = {
+      lualine_y = {
+        { "searchcount", separator = " ", padding = { left = 1, right = 0 } },
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "selectioncount", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
       lualine_z = { "encoding", "filesize", "fileformat" },
     },
   },
