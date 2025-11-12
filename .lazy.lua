c524bbd237a63d406c23974636087d3203fa3b5c
Ngô Văn Cảnh <vancanh.ng@samsung.com>
2025-11-12 15:36:35 +0700

    lazy: simplify config script

--- .lazy.lua
+++ .lazy.lua
@@ -1,11 +1,13 @@
 return {
-	"snacks.nvim",
-	opts = function(_, opts)
-		opts.picker = opts.picker or {}
-		opts.picker.sources = opts.picker.sources or {}
-		opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
-			hidden = true,
-			ignored = true,
-		})
-	end,
+  "snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
 }
