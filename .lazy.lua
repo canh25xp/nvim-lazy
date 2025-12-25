cdf541992677e44cf95436026b5d4ca882f50d29
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-12-25 19:50:48 +0700

    .lazy.lua: exclude .git when search

--- .lazy.lua
+++ .lazy.lua
@@ -6,14 +6,17 @@ return {
         explorer = {
           hidden = true,
           ignored = true,
+          exclude = { ".git" },
         },
         files = {
           hidden = true,
           ignored = true,
+          exclude = { ".git" },
         },
         grep = {
           hidden = true,
           ignored = true,
+          exclude = { ".git" },
         },
       },
     },
