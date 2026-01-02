faf598fb84a276b2e0f8010b850e02b1676cdee5
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-01-02 12:44:48 +0700

    snacks: bigfile: disable smooth scrool

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -32,6 +32,23 @@ return {
         },
       },
     },
+    bigfile = {
+      setup = function(ctx)
+        if vim.fn.exists(":NoMatchParen") ~= 0 then
+          vim.cmd([[NoMatchParen]])
+        end
+        Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+        vim.b.completion = false
+        vim.b.minianimate_disable = true
+        vim.b.minihipatterns_disable = true
+        vim.b.snacks_animate = false
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(ctx.buf) then
+            vim.bo[ctx.buf].syntax = ctx.ft
+          end
+        end)
+      end,
+    },
     dashboard = {
       preset = {
         header = logo,
