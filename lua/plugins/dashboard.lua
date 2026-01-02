98ee980724cf040d29e7038acc09d6a2e3098fa1
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-01-02 13:03:34 +0700

    snacks: dashboard: change Recent Files and Restore Session key

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,31 @@
+local logo = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+]]
+
+return {
+  "snacks.nvim",
+  opts = function(_, opts)
+    opts.dashboard = opts.dashboard or {}
+    opts.dashboard.preset = opts.dashboard.preset or {}
+    local keys = opts.dashboard.preset.keys
+    if type(keys) ~= "table" then
+      keys = {}
+      opts.dashboard.preset.keys = keys
+    end
+    opts.dashboard.preset.header = logo
+
+    -- index, item
+    for _, item in ipairs(keys) do
+      if item.desc == "Recent Files" then
+        item.key = "."
+      elseif item.desc == "Restore Session" then
+        item.key = "r"
+      end
+    end
+  end,
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,14 +1,5 @@
 local ci_selector = require("common.ci_selector")
 
-local logo = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-]]
-
 return {
   "snacks.nvim",
   opts = {
@@ -48,21 +39,6 @@ return {
           end
         end)
       end,
-    },
-    dashboard = {
-      preset = {
-        header = logo,
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = ".", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "r", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-      },
     },
     picker = {
       sources = {
