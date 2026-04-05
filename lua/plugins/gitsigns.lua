e89c86b5c27806bcf3dee755b0c582193ca6431a
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-04-05 15:43:55 +0700

    gitsigns: edit keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -22,7 +22,7 @@ return {
       local gs = package.loaded.gitsigns
 
       local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
       end
 
       map("n", "]c", function()
@@ -43,8 +43,8 @@ return {
       -- stylua: ignore start
       map("n", "]C", function() gs.nav_hunk("last") end, "Last Hunk")
       map("n", "[C", function() gs.nav_hunk("first") end, "First Hunk")
-      map({ "n", "v" }, "<leader>ghs", "<cmd>Gitsigns stage_hunk<CR>", "Stage Hunk")
-      map({ "n", "v" }, "<leader>ghr", "<cmd>Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
       map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
       map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
@@ -53,7 +53,7 @@ return {
       map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
       map("n", "<leader>ghd", gs.diffthis, "Diff This")
       map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-      map({ "o", "x" }, "ih", "<cmd><C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       -- stylua: ignore end
     end,
   },
