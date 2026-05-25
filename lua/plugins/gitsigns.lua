df3bc6fca598084103f70a6034a909d67ab3cfbb
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-05-25 14:01:51 +0700

    gitsigns: remap next, prev hunk from 'c' to 'h'

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -25,24 +25,24 @@ return {
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
       end
 
-      map("n", "]c", function()
+      map("n", "]h", function()
         if vim.wo.diff then
-          vim.cmd.normal({ "]c", bang = true })
+          vim.cmd.normal({ "]h", bang = true })
         else
           gs.nav_hunk("next")
         end
       end, "Next Hunk")
-      map("n", "[c", function()
+      map("n", "[h", function()
         if vim.wo.diff then
-          vim.cmd.normal({ "[c", bang = true })
+          vim.cmd.normal({ "[h", bang = true })
         else
           gs.nav_hunk("prev")
         end
       end, "Prev Hunk")
 
       -- stylua: ignore start
-      map("n", "]C", function() gs.nav_hunk("last") end, "Last Hunk")
-      map("n", "[C", function() gs.nav_hunk("first") end, "First Hunk")
+      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
       map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
       map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
       map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
