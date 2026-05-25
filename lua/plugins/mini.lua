bdef6fb7758f204345513c8f4e77216dcbd5c8ed
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-05-25 10:30:13 +0700

    pack: enable mini.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   "echasnovski/mini.nvim",
-  enabled = false,
+  enabled = true,
   config = function()
     -- Better Around/Inside textobjects
     -- va)  - [V]isually select [A]round [)]paren
