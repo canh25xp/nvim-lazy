341ddded34a036d7e24f5bfb4bd579499a14ec78
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-06-02 14:03:23 +0700

    catppuccin: call colorscheme lastly to avoid lualine color defect

--- init.lua
+++ init.lua
@@ -8,3 +8,5 @@ vim.g.path_sep = vim.g.is_windows and ";" or ":"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.usercommands")
+
+vim.cmd.colorscheme("catppuccin-mocha")
